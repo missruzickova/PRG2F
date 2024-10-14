84ef6a715d14f9558d4d2741035a28671a6cf705
missruzickova <lucie.ruzickova@infis.cz>
2024-10-14 13:19:48 +0200

    gggg

--- src/IF.java
+++
@@ -1,7 +0,0 @@
-import java.util.Scanner;
-
-public class IF {
-    public static void main(String[] args) {
-        Scanner cs = new Scanner(System.in)
-}
-}
---
+++ src/cykly.java
@@ -0,0 +1,61 @@
+import java.util.Random;
+
+public class cykly {
+    public static void main(String[] args) {
+
+        /*
+        //cyklus while
+
+
+        //výpis čísel 1 - 10
+
+        int cislo = 1;
+        while (cislo < 10){
+            System.out.println(cislo);
+            cislo++; //vzdy po cyklu zvětší číslo o 1
+        }
+
+         */
+
+        //kostka
+
+        Random kostka = new Random();
+
+        int cislo = kostka.nextInt(1,7);
+        while (cislo < 4){
+            cislo++;
+            System.out.println("Hodil jsi" + cislo);
+        }
+
+        do {
+            cislo++;
+            int cislo1 = kostka.nextInt(1,7);
+        }while (cislo < 4);
+
+
+        //vypiš všechny sudý čísla od 0 -1000
+
+        int csl = 1;
+
+        while (csl <= 1000){
+            System.out.println(csl);
+            csl = csl + 2;
+        }
+
+        int csl = 0;
+        while (csl <= 1000) {
+            System.out.println(csl);
+            csl += 2;
+        }
+
+        //hod kostkou, dokud nepadne 6
+
+        Random rng = new Random();
+
+        int czisllo = rng.nextInt(1,7);
+        do{
+            czi
+            System.out.println("Hodil jsi" + czisko);
+        }
+    }
+}
