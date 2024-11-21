1c8577f9bf21cf7563813c6346068bd96e4c269b
lucie ruzickova <lucie.ruzickova@infis.cz>
2024-11-21 15:25:50 +0100

    kalkulacka atd.

--- src/testovani.java
+++ src/testovani.java
@@ -6,17 +6,19 @@ public class testovani {
 
 
         int velikostMrizky = 8;
-        int ctverec = 0;
 
         //shift + f6 změní všechny proměnné a mohou se přepsat najednou
         for (int radek = 0; radek < velikostMrizky; radek++) {
             for (int sloupec = 0; sloupec < velikostMrizky; sloupec++) {
-                if (radek % 2 == 0 && sloupec % 2 != 0 || radek % != 0 && sloupec % 2 == 0) {
+                if (radek % 2 == 0 && sloupec % 2 != 0 || radek % 2 != 0 && sloupec % 2 == 0) {
                     System.out.print(" ■ ");
-                }
-                else {
+                } else {
                     System.out.print("0");
                 }
-                System.out.println();
             }
+            System.out.println();
+        }
+    }
+}
+
 
