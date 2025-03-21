1053076218039c51021c61aa951967f5d6f747c0
lucie.ruzickova <lucie.ruzickova@infis.cz>
2025-03-21 10:35:10 +0100

    unor

---
+++ src/arayListEvidenceTrid.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class arayListEvidenceTrid {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        ArrayList<ArrayList<String>> seznamTrid = new ArrayList<>();
+
+        System.out.print("Zadejte počet tříd: ");
+        int pocetTrid = sc.nextInt(); //3
+
+        for (int i = 0; i < pocetTrid; i++) {
+            seznamTrid.add(new ArrayList<>());
+
+            System.out.println("Zadejte počet žáků v " + (i + 1) + ". třídě");
+            int pocetZaku = sc.nextInt();
+            System.out.println("Zadej jména žáků:");
+
+            for (int j = 0; j < pocetZaku; j++) {
+                String zak = sc.next();
+                seznamTrid.get(i).add(zak);
+            }
+        }
+
+        System.out.println("1. třída: " + seznamTrid.get(0));
+        //seznamTrid.size(); || pocetTrid
+        for
+    }
+}
---
+++ src/testBrezen.java
@@ -0,0 +1,14 @@
+public class testBrezen {
+
+
+    public static double negativnicislo (double a) {
+
+
+       a = a * -1;
+        return a;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(negativnicislo(2));
+    }}
