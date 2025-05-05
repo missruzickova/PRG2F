1a91ffbbbf29e1b5613affa82608ee317b1b7fc5
lucie.ruzickova <lucie.ruzickova@infis.cz>
2025-05-05 13:09:39 +0200

    auta

---
+++ src/dubenStudent.java
@@ -0,0 +1,11 @@
+public class dubenStudent {
+
+    String Jméno;
+    int známky;
+
+
+    public dubenStudent(String jmeno, int znamky){
+        
+    }
+    public static void main(String[] args);
+}
