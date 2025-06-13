9d41e2aef89860651c60fadb33f96be8970cfe68
lucie.ruzickova <lucie.ruzickova@infis.cz>
2025-06-13 10:40:15 +0200

    unor

--- src/dedicnost/Teacher.java
+++ src/dedicnost/Teacher.java
@@ -3,12 +3,12 @@ package dedicnost;
 public class Teacher extends Person{
     String predmet;
     public void ucit(){
-        System.out.println("Učí na škole");}
-        @Override
-        public void introduce(){
-            super.introduce();
-            System.out.println("Učí na škole:" + predmet);
+        System.out.println("Učí na škole");
+    }
+    @Override
+    public void introduce(){
+        super.introduce();
+        System.out.println("Učí na škole:" + predmet);
 
-        }
     }
 }
